28dfa65b0c3b8da872c01b9b9c71c81e8b8bad15
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-03-22 16:00:59 +0600

    new file code

---
+++ template.js
@@ -0,0 +1,12 @@
+const first = "jaan";
+const last = "pakhi";
+const great = "potas potas ";
+
+const name = first + " " + last;
+console.log(name);
+
+const a = 10;
+const b = 20;
+
+const result = "The sum of " + a + " and " + b + " is " + (a + b);
+console.log(result);
