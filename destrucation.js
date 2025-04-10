a307e31ec0b24a7f1ee4ba1b18673d6d77c66084
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-10 22:31:41 +0600

    4th push

---
+++ destrucation.js
@@ -0,0 +1,34 @@
+// const actor = {
+//   name: "Rakib",
+//   age: 30,
+//   phone: "01978398123",
+//   money: 12250051,
+// };
+
+// const { phone, agef, money } = actor;
+// console.log(phone, age);
+// const phone = actor.phone;
+// const phone3 = actor.age;
+// console.log(phone);
+// console.log(phone3);
+// console.log(age);
+
+// const numbers = [45, 99];
+// // const [first, second] = numbers;
+// const [x, y] = [12, 66];
+
+// //
+
+// function doubleThem(a, b) {
+//   return [a * 2, b * 2];
+// }
+
+// const [first, second] = doubleThem(6, 9);
+// console.log(first, second);
+
+const myObject = {
+  student: "Rakib",
+  teacher: "milon",
+};
+
+const { student: Rafi, teacher: rakib } = myObject;
