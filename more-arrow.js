c8d8f87e528623dcbb686e32b8bc11471854f6bd
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-10 21:50:36 +0600

    eid then latest push

---
+++ more-arrow.js
@@ -0,0 +1,6 @@
+const difference = (x, y) => {
+  x - y;
+};
+const multiply = (first, second, third) => first * second * third;
+
+const getAge = (person) => person.age;
---
+++ spread.js
@@ -0,0 +1,15 @@
+// const max = Math.max(6, 23, 1, 89, 25);
+// const number = [3, 25, 54, 1, 5, 8, 5, 15, 98, 7];
+// const arrayMax = Math.max(number);
+// console.log(...number);
+
+// const nums = {"4", "5","8", "45", "6"};
+// const num2 = nums;
+// const num3 = num2.push(78);
+// console.log(...nums);
+
+const a = [1, 2];
+const b = [3, 4];
+const combined = [...a, ...b];
+
+console.log(combined); // [1, 2, 3, 4]
