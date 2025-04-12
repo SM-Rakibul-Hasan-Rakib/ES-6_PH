aca3d7cd3f0c99ffc8d19c056ed90b4cb53389aa
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-12 22:20:00 +0600

    lasted push

---
+++ class.js
@@ -0,0 +1,21 @@
+const products = [
+  { id: 1, name: "lenovo", price: 65000 },
+  { id: 2, name: "dell", price: 45000 },
+  { id: 3, name: "hp", price: 40000 },
+  { id: 4, name: "mac", price: 150000 },
+];
+
+// has some properties, methodf
+class product {
+  country = "Bangladesh";
+  constructor(name) {
+    this.name = name;
+  }
+  speak(talk) {
+    console.log(`talking about ${talk}`);
+  }
+}
+
+const lenovo = new product("Le le lenovo");
+console.log(lenovo);
+lenovo.speak("oba ki baba");
